8e270206fde68a792158730f877f300774123728
Dheerajsonekar <dheerajsonekar2@gmail.com>
2024-11-03 14:41:48 +0530

    dynamic route 1 completed

--- models/product.js
+++ models/product.js
@@ -1,36 +1,48 @@
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs');
+const path = require('path');
 
 const p = path.join(
   path.dirname(process.mainModule.filename),
-  "data",
-  "products.json"
+  'data',
+  'products.json'
 );
 
-const getProductFromFile = (cb) => {
+const getProductsFromFile = cb => {
   fs.readFile(p, (err, fileContent) => {
     if (err) {
       cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
     }
-    cb(JSON.parse(fileContent));
   });
 };
 
 module.exports = class Product {
-  constructor(t) {
-    this.title = t;
+  constructor(title, imageUrl, description, price) {
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
   }
 
   save() {
-    getProductFromFile((products) => {
+    this.id = Math.random().toString();
+    getProductsFromFile(products => {
       products.push(this);
-      fs.writeFile(p, JSON.stringify(products), (err) => {
+      fs.writeFile(p, JSON.stringify(products), err => {
         console.log(err);
       });
     });
   }
 
   static fetchAll(cb) {
-    getProductFromFile(cb);
+    getProductsFromFile(cb);
+  }
+
+  static findById(id, cb) {
+    getProductsFromFile(products => {
+      const product = products.find(p => p.id === id)
+      cb(product);
+    })
   }
 };
--- routes/admin.js
+++ routes/admin.js
@@ -2,17 +2,17 @@ const path = require('path');
 
 const express = require('express');
 
-const addProductControllers = require("../controllers/products");
+const adminController = require('../controllers/admin');
 
 const router = express.Router();
 
-
-
 // /admin/add-product => GET
-router.get('/add-product', addProductControllers.addProduct);
+router.get('/add-product', adminController.getAddProduct);
+
+// /admin/products => GET
+router.get('/products', adminController.getProducts);
 
 // /admin/add-product => POST
-router.post('/add-product',addProductControllers.postProduct);
+router.post('/add-product', adminController.postAddProduct);
 
-exports.routes = router;
-
+module.exports = router;
