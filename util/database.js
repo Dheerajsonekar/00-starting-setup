f3d405235d2449ead21dc73a40013226b4a660e2
Dheerajsonekar <dheerajsonekar2@gmail.com>
2024-11-16 12:12:58 +0530

    done with sequelize

--- util/database.js
+++ util/database.js
@@ -1,7 +1,8 @@
-const Sequelize = require("sequelize");
-const sequelize = new Sequelize("node-complete", "root", "Kalki@151", {
-  dialect: "mysql",
-  host : 'localhost'
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize('node-complete', 'root', 'Kalki@151', {
+  dialect: 'mysql',
+  host: 'localhost'
 });
 
-module.exports = sequelize
+module.exports = sequelize;
