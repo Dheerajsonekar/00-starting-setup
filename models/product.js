7e116e5b2629e1d924265e6b906ee5e95f46ccba
Dheerajsonekar <dheerajsonekar2@gmail.com>
2024-12-04 10:16:35 +0530

    final

--- models/product.js
+++ models/product.js
@@ -2,17 +2,17 @@ const Sequelize = require('sequelize');
 
 const sequelize = require('../util/database');
 
-const Product = sequelize.define('products', {
+const Product = sequelize.define('product', {
   id: {
     type: Sequelize.INTEGER,
-    allowNull: false,
     autoIncrement: true,
+    allowNull: false,
     primaryKey: true
   },
   title: Sequelize.STRING,
   price: {
-   type: Sequelize.DOUBLE,
-   allowNull: false,
+    type: Sequelize.DOUBLE,
+    allowNull: false
   },
   imageUrl: {
     type: Sequelize.STRING,
